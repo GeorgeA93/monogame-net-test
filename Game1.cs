24d30e67f244a7a3630ea9865cf74fa83d139894
George Allen <george.allen93@outlook.com>
2023-02-27 22:36:05 +0000

    Change lib

--- Game1.cs
+++ Game1.cs
@@ -7,54 +7,10 @@ using Steamworks;
 namespace SteamTest;
 
 
-public class Server : SocketManager
-{
-    public override void OnConnecting(Steamworks.Data.Connection connection, Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnConnecting(connection, info);
-        Console.WriteLine("Client Connecting to server");
-    }
-
-    public override void OnConnected(Steamworks.Data.Connection connection, Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnConnected(connection, info);
-        Console.WriteLine("Client Connected to Server");
-    }
-
-    public override void OnDisconnected(Steamworks.Data.Connection connection, Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnDisconnected(connection, info);
-        Console.WriteLine("Client Disconnected from server");
-    }
-}
-
-public class Client : ConnectionManager
-{
-    public override void OnConnected(Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnConnected(info);
-        Console.WriteLine("CLIENT CONNECT");
-    }
-
-    public override void OnConnecting(Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnConnecting(info);
-        Console.WriteLine("CLIENT CONNECTING");
-    }
-
-    public override void OnDisconnected(Steamworks.Data.ConnectionInfo info)
-    {
-        base.OnDisconnected(info);
-        Console.WriteLine("CLIENT DISCONNECT");
-    }
-}
-
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Server server;
-    private Client client;
 
     public Game1()
     {
@@ -67,31 +23,20 @@ public class Game1 : Game
     {
 
         base.Initialize();
-
-        SteamClient.Init(1442410, false);
-        SteamNetworkingUtils.OnDebugOutput += (output, str) => {
-            Console.WriteLine(str);
-        };
-        Steamworks.Dispatch.OnDebugCallback = (type, str, server) => {
-            Console.WriteLine( $"[Callback {type} {(server ? "server" : "client")}]" );
-			Console.WriteLine( str );
-			Console.WriteLine( $"" );
-        };
-        Steamworks.Dispatch.OnException = (e) =>
-        {
-            Console.Error.WriteLine( e.Message );
-			Console.Error.WriteLine( e.StackTrace );
-            throw e;
-        };
-        SteamNetworkingUtils.DebugLevel = NetDebugOutput.Everything;
-        SteamNetworkingUtils.DebugLevelP2PRendezvous = 8;
-        SteamNetworkingUtils.DebugLevelMessage = 8;
-        SteamNetworkingUtils.DebugLevelSDRRelayPings = 8;
-        SteamNetworkingUtils.DebugLevelAckRTT = 8;
-        SteamNetworkingUtils.DebugLevelPacketDecode = 8;
-        SteamNetworkingUtils.DebugLevelPacketGaps = 8;
-
+        SteamAPI.Init();
         SteamNetworkingUtils.InitRelayNetworkAccess();
+
+        Callback<SteamNetConnectionStatusChangedCallback_t>.Create((foo) =>
+        {
+            Console.WriteLine(foo.m_info.m_eState);
+            SteamNetworkingSockets.AcceptConnection(foo.m_hConn);
+        });
+
+        SteamNetworkingSockets.CreateListenSocketP2P(0, 0, null);
+        var id = SteamUser.GetSteamID();
+        SteamNetworkingIdentity fo = new SteamNetworkingIdentity();
+        fo.SetSteamID(id);
+        SteamNetworkingSockets.ConnectP2P(ref fo,0, 0, null);
     }
 
     protected override void LoadContent()
@@ -102,47 +47,20 @@ public class Game1 : Game
     protected override void UnloadContent()
     {
         base.UnloadContent();
-        if (server != null)
-        {
-            server.Close();
-        }
-        if (client != null)
-        {
-            client.Close();
-        }
-        SteamClient.Shutdown();
+        SteamAPI.Shutdown();
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        {
             Exit();
-
-        SteamClient.RunCallbacks();
-
-        if (server == null)
-        {
-            if (SteamNetworkingUtils.Status == SteamNetworkingAvailability.Current)
-            {
-                Console.WriteLine("Connected to steam relay - starting server");
-                server = SteamNetworkingSockets.CreateRelaySocket<Server>();
-                Console.WriteLine("Started server");
-
-                Console.WriteLine("Connecting to server");
-                client = SteamNetworkingSockets.ConnectRelay<Client>(Steamworks.SteamClient.SteamId);
-            }
-        }
-
-        if (server != null)
-        {
-            server.Receive();
-        }
-        if (client != null)
-        {
-            client.Receive();
         }
 
         base.Update(gameTime);
+
+        SteamAPI.RunCallbacks();
+
     }
 
     protected override void Draw(GameTime gameTime)
